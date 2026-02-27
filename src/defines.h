122306a8662e76990dd29684b32348818bc89830
Alexander Young <alex1001@live.ca>
2026-02-27 16:55:51 -0700

    Updates IMU default configuration
    Adjusts the default pin for the IMU heater to 4.
    Disables the hardcoded primary IMU I2C address, allowing for more flexible address definition.

--- src/defines.h
+++ src/defines.h
@@ -51,7 +51,7 @@
 
 // Set I2C address here or directly in IMU_DESC_ENTRY for each IMU used
 // If not set, default address is used based on the IMU and Sensor ID
-#define PRIMARY_IMU_ADDRESS_ONE 0x6b
+// #define PRIMARY_IMU_ADDRESS_ONE 0x6b
 // #define SECONDARY_IMU_ADDRESS_TWO 0x4b
 
 #ifndef BATTERY_MONITOR
